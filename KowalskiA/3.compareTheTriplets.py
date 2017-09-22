2982f9bfa83bc8abc2d88667fb764065c825d7c2
KowalskiA <31545979+KowalskiA@users.noreply.github.com>
2017-09-22 19:21:48 +0530

    Create 3.compareTheTriplets.py

---
+++ KowalskiA/3.compareTheTriplets.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import sys
+
+def solve(a0, a1, a2, b0, b1, b2):
+    A=0
+    B=0
+    if a0>b0:
+        A=A+1
+    elif b0>a0:
+         B=B+1
+    else:
+        A=0
+        B=0
+    if a1>b1:
+        A=A+1
+    elif b1>a1:
+         B=B+1
+        
+    if a2>b2:
+        A=A+1
+    elif b2>a2:
+         B=B+1  
+    return(A,B)
+a0, a1, a2 = input().strip().split(' ')
+a0, a1, a2 = [int(a0), int(a1), int(a2)]
+b0, b1, b2 = input().strip().split(' ')
+b0, b1, b2 = [int(b0), int(b1), int(b2)]
+result = solve(a0, a1, a2, b0, b1, b2)
+print (" ".join(map(str, result)))
